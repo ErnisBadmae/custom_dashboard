064f6bf2711033984717e88c038c0b60a4f7b42a
Ernis <e.badmaev@gmail.com>
2022-04-27 10:54:15 +0300

    chart

--- src/components/featured/Featured.jsx
+++ src/components/featured/Featured.jsx
@@ -1,5 +1,5 @@
 import './featured.scss';
-import { MoreVert } from '@mui/icons-material';
+import { MoreVert, KeyboardArrowDown, KeyboardArrowUp } from '@mui/icons-material';
 import { CircularProgressbar } from 'react-circular-progressbar';
 import 'react-circular-progressbar/dist/styles.css';
 
@@ -19,6 +19,28 @@ function Featured(props) {
         <p className="desc">
           Previous transactions proccesing. Last payments may not be included.
         </p>
+        <div className="summary">
+          <div className="item">
+            <div className="itemTitle">Target</div>
+            <div className="itemResult negative">
+              <KeyboardArrowDown fontSize='small' />
+              <div className="resultAmount">$12.4k</div>
+            </div>
+          </div>
+          <div className="item">
+            <div className="itemTitle">Last week</div>
+            <div className="itemResult negative">
+              <KeyboardArrowDown fontSize='small' />
+              <div className="resultAmount">$12.4k</div>
+            </div>
+          </div> <div className="item">
+            <div className="itemTitle">Last month</div>
+            <div className="itemResult positive">
+              <KeyboardArrowUp fontSize='small' />
+              <div className="resultAmount">$12.4k</div>
+            </div>
+          </div>
+        </div>
       </div>
     </div>
   );
